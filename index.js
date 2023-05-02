61b06a6d0b63c306900d9a50b3bda1d4dbcf4d8f
Shaon Imdad <shaonimdad7@gmail.com>
2023-05-03 04:12:05 +0600

    catagory json updated

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/catagory.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Dragon is coming')
