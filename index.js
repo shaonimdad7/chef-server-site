16f628ef8a41b1bff7bcb3964e866a07b51e52c8
Shaon Imdad <shaonimdad7@gmail.com>
2023-05-03 01:02:09 +0600

    this is another site

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+
+const categories = require('./data/catagory.json');
+
+app.get('/', (req, res) => {
+    res.send('Dragon is coming')
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
+})
